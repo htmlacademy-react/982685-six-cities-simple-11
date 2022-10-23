af268a1d81e8411aca076ddda51aa016d9dcaa20
Somov Eugene <somov.eugene.2012@gmail.com>
2022-10-23 20:05:19 +0300

    Добавляет в компонент карточки предложения props и отрисовывает их.

--- project/src/components/place-card/place-card.tsx
+++ project/src/components/place-card/place-card.tsx
@@ -1,31 +1,35 @@
-function PlaceCard(): JSX.Element {
+import { PlaceCardInfo } from '../../types/types';
+
+type PlaceCardProps = Omit<PlaceCardInfo, 'id'>;
+
+function PlaceCard({ mark, imageSrc, imageAlt, priceValue, priceText, ratingStars, name, type }: PlaceCardProps): JSX.Element {
   return (
     <article className="cities__card place-card">
       <div className="place-card__mark">
-        <span>Premium</span>
+        <span>{mark}</span>
       </div>
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#dummy">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Apartment" />
+          <img className="place-card__image" src={imageSrc} alt={imageAlt} width="260" height="200" />
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;120</b>
-            <span className="place-card__price-text">&#47;&nbsp;night</span>
+            <b className="place-card__price-value">&euro;{priceValue}</b>
+            <span className="place-card__price-text">&nbsp;&#47;&nbsp;{priceText}</span>
           </div>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: '80%'}}></span>
+            <span style={{width: `${20 * ratingStars}%`}}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#dummy">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="#dummy">{name}</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>
   );
