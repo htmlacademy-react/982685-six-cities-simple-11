8ed558f9929554ce7cf409e9a0f41f9e7102f1a4
Somov Eugene <somov.eugene.2012@gmail.com>
2022-10-25 22:30:50 +0300

    Создаёт компонент для страницы «404 Page not-found».

---
+++ project/src/pages/not-found/not-found.tsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
+function NotFound(): JSX.Element {
+  return (
+    <div className="page page--gray page--login">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link" href="main.html">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+          <section className="login">
+            <h1 className="login__title">Error 404: Page not found</h1>
+          </section>
+          <section className="locations locations--login locations--current">
+            <div className="locations__item">
+              <Link className="locations__item-link" to={AppRoute.Root}>
+                <span>Go to homepage</span>
+              </Link>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default NotFound;
