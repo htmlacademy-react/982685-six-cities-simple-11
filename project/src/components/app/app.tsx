0e95ed7b76d8f711cd2ec2ed3529e6828a7d8a51
Somov Eugene <somov.eugene.2012@gmail.com>
2022-10-25 23:27:36 +0300

    Добавляет маршрутизацию.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,10 @@
-import Main from '../../pages/main/main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { City, PlaceCardInfo } from '../../types/types';
+import { AppRoute } from '../../const';
+import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Property from '../../pages/property/property';
+import NotFound from '../../pages/not-found/not-found';
 
 type AppProps = {
   city: City;
@@ -9,11 +14,32 @@ type AppProps = {
 
 function App({ city, rentalOffers, places }: AppProps): JSX.Element {
   return (
-    <Main
-      city={city}
-      rentalOffers={rentalOffers}
-      places={places}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={
+            <Main
+              city={city}
+              rentalOffers={rentalOffers}
+              places={places}
+            />
+          }
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<Property />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
