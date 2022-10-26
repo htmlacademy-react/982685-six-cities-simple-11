b2944d75b6ea9e9be7cd68907b10588e9b1ddf04
Somov Eugene <somov.eugene.2012@gmail.com>
2022-10-26 21:50:36 +0300

    Обновляет ссылку для логотипа.

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -1,3 +1,4 @@
+import Logo from '../../components/logo/logo';
 import PlaceCard from '../../components/place-card/place-card';
 import { City, PlaceCardInfo} from '../../types/types';
 
@@ -13,11 +14,7 @@ function Main({ city, rentalOffers, places }: MainProps): JSX.Element {
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active" href="#dummy">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
+            <Logo />
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
--- project/src/pages/not-found/not-found.tsx
+++ project/src/pages/not-found/not-found.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../types/types';
+import Logo from '../../components/logo/logo';
 
 function NotFound(): JSX.Element {
   return (
@@ -7,11 +8,7 @@ function NotFound(): JSX.Element {
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
+            <Logo />
           </div>
         </div>
       </header>
