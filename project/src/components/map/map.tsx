26cc36fcf380833f486a112b4f674ad61a04a9b1
Somov Eugene <somov.eugene.2012@gmail.com>
2022-11-04 15:21:02 +0300

    Добавляет компонент «Карта» и пользовательский hook.

---
+++ project/src/components/map/map.tsx
@@ -0,0 +1,39 @@
+import { useRef, useEffect } from 'react';
+import { Icon, Marker } from 'leaflet';
+import useMap from '../../hooks/useMap';
+import { City, Points } from '../../types/types';
+import { MARKER_DEFAULT_URL} from '../../const';
+import 'leaflet/dist/leaflet.css';
+
+type MapProps = {
+  city: City;
+  points: Points;
+};
+
+const defaultCustomIcon = new Icon({
+  iconUrl: MARKER_DEFAULT_URL,
+  iconSize: [27, 39],
+  iconAnchor: [13, 39],
+});
+
+function Map({ city, points }: MapProps): JSX.Element {
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+
+  useEffect(() => {
+    if (map) {
+      points.forEach((point) => {
+        const marker = new Marker({
+          lat: point.lat,
+          lng: point.lng,
+        });
+
+        marker.setIcon(defaultCustomIcon).addTo(map);
+      });
+    }
+  }, [map, points]);
+
+  return <section className="cities__map map" ref={mapRef}></section>;
+}
+
+export default Map;
