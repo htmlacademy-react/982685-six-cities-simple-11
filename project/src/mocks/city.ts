137abf5c35040154fbf108a84d4bb031c8556fea
Somov Eugene <somov.eugene.2012@gmail.com>
2022-11-06 16:13:57 +0300

    Исправляет моковые данные для объектов аренды.

--- project/src/mocks/city.ts
+++ project/src/mocks/city.ts
@@ -1,8 +1,10 @@
 import { City } from '../types/types';
 
 export const CITY: City = {
-  title: 'Amsterdam',
-  lat: 52.370216,
-  lng: 4.895168,
-  zoom: 12
+  name: 'Amsterdam',
+  location: {
+    latitude: 52.370216,
+    longitude: 4.895168,
+    zoom: 12,
+  },
 };
