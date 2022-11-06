2bcaa910a478220c85c63c994bec2507c4b09a31
Somov Eugene <somov.eugene.2012@gmail.com>
2022-11-06 20:54:40 +0300

    Добавляет новый отзыв в моковые данные.

--- project/src/mocks/reviews.ts
+++ project/src/mocks/reviews.ts
@@ -10,8 +10,8 @@ export const mockReviews: ReviewsType = [
       avatarUrl: 'https://11.react.pages.academy/static/avatar/3.jpg'
     },
     rating: 4,
-    comment: 'We loved it so much, the house, the veiw, the location just great.. Highly reccomend :)',
-    date: '2022-10-16T13:58:46.495Z'
+    comment: 'Bathed in the nature. Completely unplugged. Unforgettable.',
+    date: '2022-08-16T13:58:46.495Z'
   },
   {
     id: 2,
@@ -21,8 +21,20 @@ export const mockReviews: ReviewsType = [
       name: 'Emely',
       avatarUrl: 'https://11.react.pages.academy/static/avatar/8.jpg'
     },
-    rating: 3,
+    rating: 5,
     comment: 'We loved it so much, the house, the veiw, the location just great.. Highly recommend :)',
-    date: '2022-10-10T13:58:46.499Z'
+    date: '2022-09-10T13:58:46.499Z'
+  },
+  {
+    id: 3,
+    user: {
+      id: 18,
+      isPro: true,
+      name: 'Sophie',
+      avatarUrl: 'https://11.react.pages.academy/static/avatar/9.jpg'
+    },
+    rating: 4,
+    comment: 'What an amazing view! The house is stunning and in an amazing location. The large glass wall had an amazing view of the river!',
+    date: '2022-10-06T13:58:46.499Z'
   }
 ];
