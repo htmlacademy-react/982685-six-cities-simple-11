a7ef57b13433e11102e84f6f6dc29e4d26ee463d
Somov Eugene <somov.eugene.2012@gmail.com>
2022-11-06 20:43:41 +0300

    Обновляет компонент страницы «Property» и отрисовывает список отзывов с помощью компонента «Список отзывов».

--- project/src/pages/property/property.tsx
+++ project/src/pages/property/property.tsx
@@ -1,6 +1,8 @@
 import { Helmet } from 'react-helmet-async';
 import Logo from '../../components/logo/logo';
-import ReviewsForm from '../../components/reviews-form/reviews-form';
+import ListReviews from '../../components/list-reviews/list-reviews';
+import FormReview from '../../components/form-review/form-review';
+import { mockReviews } from '../../mocks/reviews';
 
 function Property(): JSX.Element {
   return (
@@ -146,32 +148,8 @@ function Property(): JSX.Element {
                 </div>
               </div>
               <section className="property__reviews reviews">
-                <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
-                <ul className="reviews__list">
-                  <li className="reviews__item">
-                    <div className="reviews__user user">
-                      <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
-                      </div>
-                      <span className="reviews__user-name">
-                        Max
-                      </span>
-                    </div>
-                    <div className="reviews__info">
-                      <div className="reviews__rating rating">
-                        <div className="reviews__stars rating__stars">
-                          <span style={{width: '80%'}}></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <p className="reviews__text">
-                        A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                      </p>
-                      <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                    </div>
-                  </li>
-                </ul>
-                <ReviewsForm />
+                <ListReviews reviews={mockReviews} />
+                <FormReview />
               </section>
             </div>
           </div>
