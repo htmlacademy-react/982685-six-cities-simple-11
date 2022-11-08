91e046bd479c58c8949259666b52077c0ee52a1d
Somov Eugene <somov.eugene.2012@gmail.com>
2022-11-08 23:50:34 +0300

    Переименовывает и группирует константы в соответствии с критерием Д3.

--- project/src/const.ts
+++ project/src/const.ts
@@ -1,15 +1,30 @@
-// Rating (star)
-export const RATING_UNDEFINED = 0;
-export const RATING_MIN = 1;
-export const RATING_MAX = 5;
+// Names block for place cards
+export const BlockPlaces = {
+  Cities: 'cities',
+  NearPlaces: 'near-places',
+} as const;
 
-// Reviews
-export const REVIEW_MIN = 50;
-export const REVIEW_MAX = 300;
+// Rating (star)
+export const Rating = {
+  Undefined: 0,
+  MinStars: 1,
+  MaxStars: 5
+} as const;
+
+// Review
+export const Review = {
+  MinLength: 50,
+  MaxLength: 300
+} as const;
 
 // Leaflet Map
-export const LEAFLET_URL = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
-export const LEAFLET_ATTRIBUTION =
-  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
-export const MARKER_DEFAULT_URL = 'img/pin.svg';
-export const MARKER_ACTIVE_URL = 'img/pin-active.svg';
+export const Leaflet = {
+  MapUrl: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+  Attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  MarkerDefaultUrl: 'img/pin.svg',
+  MarkerActiveUrl: 'img/pin-active.svg',
+  HeightMap: {
+    Main: 'calc(100vh - 186px)',
+    Property: '579px'
+  }
+} as const;
