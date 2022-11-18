92c0e51bd88a1431a8e762469e23368af699ae1f
Somov Eugene <somov.eugene.2012@gmail.com>
2022-11-19 00:48:35 +0300

    Подключает компонент «Варианты сортировки» к главной странице.

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -3,15 +3,19 @@ import { useAppSelector } from '../../hooks';
 import Logo from '../../components/logo/logo';
 import ListCities from '../../components/list-cities/list-cities';
 import ListOffers from '../../components/list-offers/list-offers';
+import SortingOptions from '../../components/sorting-options/sorting-options';
 import Map from '../../components/map/map';
 import MainEmpty from '../main-empty/main-empty';
 import { BlockPlaces, Leaflet } from '../../const';
+import { getOffersByCity } from '../../utils/utils';
+import sortOffers from '../../utils/sort-offers';
 
 function Main(): JSX.Element {
   const currentCity = useAppSelector((state) => state.city);
   const allOffers = useAppSelector((state) => state.offers);
+  const sortType = useAppSelector((state) => state.sortOffers);
 
-  const cityOffers = allOffers.filter((offer) => offer.city.name === currentCity.name);
+  const cityOffers = getOffersByCity(allOffers, currentCity);
   const numberOffers = cityOffers.length;
   const isOffers = (numberOffers > 0);
 
@@ -44,7 +48,7 @@ function Main(): JSX.Element {
         </div>
       </header>
 
-      <main className={`page__main page__main--index${!isOffers ? ' page__main--index-empty' : ''}`}>
+      <main className={`page__main page__main--index${isOffers ? '' : ' page__main--index-empty'}`}>
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
@@ -57,22 +61,9 @@ function Main(): JSX.Element {
               <section className="cities__places places">
                 <h2 className="visually-hidden">Places</h2>
                 <b className="places__found">{numberOffers} places to stay in {currentCity.name}</b>
-                <form className="places__sorting" action="#" method="get">
-                  <span className="places__sorting-caption">Sort by</span>{' '}
-                  <span className="places__sorting-type" tabIndex={0}>Popular
-                    <svg className="places__sorting-arrow" width="7" height="4">
-                      <use xlinkHref="#icon-arrow-select"></use>
-                    </svg>
-                  </span>
-                  <ul className="places__options places__options--custom places__options--opened">
-                    <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                    <li className="places__option" tabIndex={0}>Price: low to high</li>
-                    <li className="places__option" tabIndex={0}>Price: high to low</li>
-                    <li className="places__option" tabIndex={0}>Top rated first</li>
-                  </ul>
-                </form>
+                <SortingOptions />
                 <div className="cities__places-list places__list tabs__content">
-                  <ListOffers block={BlockPlaces.Cities} offers={cityOffers} />
+                  <ListOffers block={BlockPlaces.Cities} offers={sortOffers(cityOffers, sortType)} />
                 </div>
               </section>
               <div className="cities__right-section">
