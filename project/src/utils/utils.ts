bd96979c533914f1c3f16606f778ba340a327c61
Somov Eugene <somov.eugene.2012@gmail.com>
2022-11-19 00:46:03 +0300

    Добавляет сортировку карточек с предложениями.

--- project/src/utils/utils.ts
+++ project/src/utils/utils.ts
@@ -1,8 +1,19 @@
 import { Rating } from '../const';
+import { City, Offers } from '../types/types';
 
 /**
  * Calculates the width of block 'rating__star'
  * @param {number} rating - The rating from 1 to 5.
  * @returns {number} Width of block in percent.
  */
-export const getWidthRating = (rating: number): number => Math.round((100 / Rating.MaxStars) * rating);
+export const getWidthRating = (rating: number): number =>
+  Math.round((100 / Rating.MaxStars) * rating);
+
+/**
+ *
+ * @param {Offers} offers - All offers
+ * @param {City} city - The city to be searched
+ * @returns {Offers} Filtred list of offers by city
+ */
+export const getOffersByCity = (offers: Offers, city: City): Offers =>
+  offers.filter((offer) => offer.city.name === city.name);
