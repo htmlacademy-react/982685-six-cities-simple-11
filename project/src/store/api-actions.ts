17a169ae678399955dc6ddf6049251c60a47ac44
Somov Eugene <somov.eugene.2012@gmail.com>
2022-11-20 13:43:24 +0300

    Добавляет действие для проверки прохождения авторизации.

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -1,14 +1,39 @@
 import { AxiosInstance } from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { setOffersAction } from './actions';
+import { requireAuthorization, setOffersAction } from './actions';
 import { AppDispatch, State } from '../types/state';
-import { APIRoute, Offers } from '../types/types';
+import { APIRoute, AuthorizationStatus, Offers } from '../types/types';
 
 export const fetchOffersAction = createAsyncThunk<
   void,
   undefined,
-  { dispatch: AppDispatch; state: State; extra: AxiosInstance }
->('data/fetchQuestions', async (_arg, { dispatch, extra: api }) => {
-  const { data } = await api.get<Offers>(APIRoute.Offers);
-  dispatch(setOffersAction(data));
-});
+  {
+    dispatch: AppDispatch;
+    state: State;
+    extra: AxiosInstance;
+  }
+>('data/fetchQuestions',
+  async (_arg, { dispatch, extra: api }) => {
+    const { data } = await api.get<Offers>(APIRoute.Offers);
+    dispatch(setOffersAction(data));
+  }
+);
+
+export const checkAuthAction = createAsyncThunk<
+  void,
+  undefined,
+  {
+    dispatch: AppDispatch;
+    state: State;
+    extra: AxiosInstance;
+  }
+>('user/checkAuth',
+  async (_arg, { dispatch, extra: api }) => {
+    try {
+      await api.get(APIRoute.Login);
+      dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    } catch {
+      dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+    }
+  }
+);
