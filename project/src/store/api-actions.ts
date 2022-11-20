65d05b084560a980b51c2fe3197a96687221db9e
Somov Eugene <somov.eugene.2012@gmail.com>
2022-11-20 13:55:21 +0300

    Добавляет новые асинхронные действия для авторизации - Login и Logout.

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -1,8 +1,9 @@
 import { AxiosInstance } from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { requireAuthorization, setOffersAction } from './actions';
+import { dropToken, saveToken } from '../services/token';
 import { AppDispatch, State } from '../types/state';
-import { APIRoute, AuthorizationStatus, Offers } from '../types/types';
+import { APIRoute, AuthData, AuthorizationStatus, Offers, UserData } from '../types/types';
 
 export const fetchOffersAction = createAsyncThunk<
   void,
@@ -35,5 +36,38 @@ export const checkAuthAction = createAsyncThunk<
     } catch {
       dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
     }
+  }
+);
+
+export const loginAction = createAsyncThunk<
+  void,
+  AuthData,
+  {
+    dispatch: AppDispatch;
+    state: State;
+    extra: AxiosInstance;
+  }
+>(
+  'user/login',
+  async ({ login: email, password }, { dispatch, extra: api }) => {
+    const { data: { token } } = await api.post<UserData>(APIRoute.Login, { email, password });
+    saveToken(token);
+    dispatch(requireAuthorization(AuthorizationStatus.Auth));
+  }
+);
+
+export const logoutAction = createAsyncThunk<
+  void,
+  undefined,
+  {
+    dispatch: AppDispatch;
+    state: State;
+    extra: AxiosInstance;
+  }
+>('user/logout',
+  async (_arg, { dispatch, extra: api }) => {
+    await api.delete(APIRoute.Logout);
+    dropToken();
+    dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
   }
 );
