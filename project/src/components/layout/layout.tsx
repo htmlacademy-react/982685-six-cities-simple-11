182e6e583865bfa8b23c387b636f6c3e386fc94d
Somov Eugene <somov.eugene.2012@gmail.com>
2022-11-25 12:30:33 +0300

    Добавляет компонент Layout и переносит в него header

---
+++ project/src/components/layout/layout.tsx
@@ -0,0 +1,58 @@
+import { MouseEvent } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import Logo from '../../components/logo/logo';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { logoutAction } from '../../store/api-actions';
+import { AppRoute, AuthorizationStatus } from '../../types/types';
+
+function Layout(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
+  const handleLogoutClick = (evt: MouseEvent<HTMLAnchorElement>): void => {
+    evt.preventDefault();
+    dispatch(logoutAction());
+  };
+
+  return (
+    <>
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <Logo />
+            <nav className="header__nav">
+              {authorizationStatus === AuthorizationStatus.Auth ? (
+                <ul className="header__nav-list">
+                  <li className="header__nav-item user">
+                    <div className="header__nav-profile">
+                      <div className="header__avatar-wrapper user__avatar-wrapper" />
+                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                    </div>
+                  </li>
+                  <li className="header__nav-item">
+                    <Link className="header__nav-link" to={AppRoute.Root} onClick={handleLogoutClick}>
+                      <span className="header__signout">Sign out</span>
+                    </Link>
+                  </li>
+                </ul>
+              ) : (
+                <ul className="header__nav-list">
+                  <li className="header__nav-item user">
+                    <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Login}>
+                      <div className="header__avatar-wrapper user__avatar-wrapper" />
+                      <span className="header__login">Sign in</span>
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      <Outlet />
+    </>
+  );
+}
+
+export default Layout;
