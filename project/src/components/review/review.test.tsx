2b323c7bfc535bcc9596533a4fe6ae26d0300320
Somov Eugene <somov.eugene.2012@gmail.com>
2022-12-03 15:53:43 +0300

    Добавляет тесты для компонентов.

---
+++ project/src/components/review/review.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import { mockReview } from '../../utils/mocks';
+import HistoryRouter from '../history-route/history-route';
+import { createMemoryHistory } from 'history';
+import Review from './review';
+
+const review = mockReview();
+const history = createMemoryHistory();
+
+describe('Component: Review', () => {
+  it('should render correctly', () => {
+    render(
+      <HistoryRouter history={history}>
+        <Review review={review} />
+      </HistoryRouter>
+    );
+
+    expect(screen.getByRole('img')).toBeInTheDocument();
+    expect(screen.getByRole('img')).toHaveClass('reviews__avatar');
+    expect(screen.getByText(review.user.name)).toBeInTheDocument();
+    expect(screen.getByText(/Rating/i)).toBeInTheDocument();
+  });
+});
